9d6ac10fce197e3b1b8d57c7ba999d4830d813c0
teena-batra <teenabatra01@gmail.com>
2022-11-05 16:44:06 +0530

    swap and count digit

--- Count_Digits/Main.java
+++ Count_Digits/Main.java
@@ -1,17 +1,35 @@
 package Count_Digits;
-
+import java.util.Scanner;
 public class Main {
 
     public static void countDigits1(int n) {
-
+    int   count=0;
+    while (n>0) {
+        n /= 10;
+        count++;
+    }
+    System.out.println(count);
     }
     public static void countDigits2(int n) {
-        
+        int count = 0;
+        do {
+            count++;
+            n /= 10;
+            
+        } while (n>0);
+        System.out.println(count);
     }
     public static void countDigits3(int n) {
-        
+        int count = 0;
+        for (int index = n; index>0; index/=10) {
+            count++;
+        }
+        System.out.println(count);
     }
     public static void main(String[] args) {
-      
+        int n;
+        Scanner obj = new Scanner(System.in);
+        n = obj.nextInt();
+        countDigits3(n);
     }
 }
--- Swap_Numbers/Main.java
+++ Swap_Numbers/Main.java
@@ -2,6 +2,15 @@ package Swap_Numbers;
 
 public class Main {
     public static void main(String[] args) {
-        
+        swap(5,6);
     }
+    static void swap(int a,int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("value of a is: " + a);
+        System.out.println("value of b is: " + b);
+    }
+
+       
 }
