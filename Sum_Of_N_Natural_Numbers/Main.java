7e06fa683440713933a2362f0fa1d910b0150829
Faraz-mobin17 <faraz.mobin17@gmail.com>
2022-11-05 15:45:43 +0530

    changed file in sum of n natural numbers

--- Sum_Of_N_Natural_Numbers/Main.java
+++ Sum_Of_N_Natural_Numbers/Main.java
@@ -2,6 +2,11 @@ package Sum_Of_N_Natural_Numbers;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int sum = 0;
+        int n = 10;
+        for (int i = 1; i <= n; i += 1) {
+            sum += i;
+        }
+        System.out.println(sum);
     }
 }
