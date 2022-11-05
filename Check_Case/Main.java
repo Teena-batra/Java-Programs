27e5fd6e76acf515fccb5d9cb46fbbcf25006dbe
teena-batra <teenabatra01@gmail.com>
2022-11-05 16:12:47 +0530

    added code

--- Check_Case/Main.java
+++ Check_Case/Main.java
@@ -1,7 +1,16 @@
 package Check_Case;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        char a;
+        Scanner obj = new Scanner(System.in);
+        a = obj.next().charAt(0);
+        if(a>='a' && a<='z')
+        System.out.println("lower case");
+        else if(a>='A' && a<='Z')
+        System.out.println("upper case");
+        else 
+        System.out.println("not a character");
+                
     }
 }
