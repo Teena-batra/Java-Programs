99eee479fb761d93c36bdff9956761b6c93604e5
teena-batra <teenabatra01@gmail.com>
2022-11-07 00:20:46 +0530

    fartocel and celtofar

--- Temperature_Convert/Main.java
+++ Temperature_Convert/Main.java
@@ -1,7 +1,27 @@
 package Temperature_Convert;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        int F,C;
+        Scanner obj1 = new Scanner(System.in);
+        C = obj1.nextInt();
+        Scanner obj2 = new Scanner(System.in);
+        F = obj2.nextInt();
+        FarToCel(F);
+        CelToFar(C);
     }
+      static  void FarToCel( int F){
+            int C1 = 5*(F-32)/9;
+            System.out.println(+C1);
+
+        }
+        static void CelToFar(int C){
+         int F1 = C*9/5+32;
+         System.out.println(F1);
+        }
+
+
+        
+        
+    
 }
